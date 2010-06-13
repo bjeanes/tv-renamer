533e74c476be8afd274a5f0128e7ee06a1d06371
Jason Weathered <git@jason.backtick.org>
2010-06-13 17:08:09 +0800

    Include subtitles (.srt) files in rename.

--- tv-renamer.rb
+++ tv-renamer.rb
@@ -4,7 +4,7 @@ require 'lib/tvdb/tvdb'
 require 'lib/tvparser'
 require 'lib/tvrenamer'
 
-VALID_VIDEO_TYPES = %w{avi mkv m2v mpg mpeg mov ogm wmv mp4}
+VALID_VIDEO_TYPES = %w{avi mkv m2v mpg mpeg mov ogm wmv mp4 srt}
 QUALITIES = %w{HDTV 720P 1080i Bluray}
 
 renamer = Renamer.new(ARGV.shift || ".")
